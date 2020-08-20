4b9144bfd73150bf476cd595c8db6de493c2129c
w1nst0nxu <winstonx2004@gmail.com>
2020-08-20 10:49:45 -0700

    Added the text for when button = 10

---
+++ app/src/main/java/com/example/buttonclicker2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.buttonclicker2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private var timesClicked = 0
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun onClickMe(view : View) {
+            timesClicked++
+            button.setText(timesClicked.toString())
+
+            if(timesClicked == 10) {
+                Toast.makeText(this, "Only 10???", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
